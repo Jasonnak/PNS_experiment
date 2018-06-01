0b66776a0ce820eb469baa504e574abd8f277c47
Jasonnak <Jasonnak@hotmail.com>
2018-06-01 11:19:08 +0200

    Logging added, new comments

--- PNS_project.py
+++ PNS_project.py
@@ -5,17 +5,23 @@ Created on Sun May 13 17:38:30 2018
 @author: jason
 """
 
+## If the experiment does not run optimally, you might want to consider downgrading pyhton packages.
+## The main package versions are listed below
+
+## Psychopy version 1.85.3
 from psychopy.visual import Window, TextStim  
 from psychopy.event import waitKeys, clearEvents
 from psychopy.core import wait
+## pandas version 0.22.0
 import pandas as pd
 import os
+## python-pygaze 0.6.0a25
 from pygaze.sound import Sound
 
 
-## calling directory; Change before running
+## calling directory; Change before running!!!!
 os.chdir('D:/Muy importante/resmas/ProgrammingTNS')
-
+## participant number
 pp = 'test'
 ## Some initial settings
 DISPSIZE = [1536,864]
@@ -49,20 +55,22 @@ def ab(difficulty,ability,outcome,K = .5):
 
 ## starting ability
 ability = .5
+## amount of trials
 trials = 20
 
-
+## initiate welcome text
 welcome.draw()
 win.flip()
 waitKeys()
 win.flip()
 wait(2)
 
+## open log for participant data storage
 log = open(str(pp) + "_data.txt",'w')
 log.write("pp\ttrial\tid\toutcome\tdif\tinterim_ability\n")
 
 for j in range(trials):
-    ## looking for min dif in  ability
+    ## looking for min dif in ability
     diflist = abs(ability - dat['difficulty'])
     index = min( (diflist[diflist.index[i]],i) for i in range(len(diflist)) )
     ind = index[1]
@@ -99,15 +107,16 @@ for j in range(trials):
             outcome = 1
             correct.draw()
             win.flip()
-    ## calculating new abilities/difficulties
+    ## calculating new abilities/difficulties and logging participant data
     ability1 = ab(difficulty = dat['difficulty'][dat.index[ind]],ability = ability,outcome = outcome)
     logid = dat['id'][dat.index[ind]]
     logdif = dat['difficulty'][dat.index[ind]]
     log.write(str(pp) + '\t' + str(j) + '\t' + str(logid) + '\t' + str(outcome) + "\t" + str(logdif) + "\t" + str(ability1) + "\n")
     dat['difficulty'][dat.index[ind]] = dif(difficulty = dat['difficulty'][dat.index[ind]],ability = ability,outcome = outcome)
     ability = ability1
+    ## clear buttonpress history
     clearEvents(eventType='keyboard')
-    ## storing items in new dataframe and dropping them from old so not reused
+    ## storing items in new dataframe and dropping them from old so they are not reused
     data = data.append(dat.loc[dat.index[ind]])
     dat = dat.drop(dat.index[ind])
     wait(.5)
@@ -116,7 +125,7 @@ for j in range(trials):
 df = dat.append(data)
 df.to_csv('diflist.csv', sep=',', index=False)
 log.close()
-abtext = TextStim(win,text = "Your overall ability for this task was:"+str(round(ability,2)) + "\nThank you for playing.",  height = 30, color = FGC, wrapWidth = 1000)
+abtext = TextStim(win,text = "Your overall ability for this task was:" + str(round(ability,2)) + "\nThank you for playing.",  height = 30, color = FGC, wrapWidth = 1000)
 abtext.draw()
 win.flip()
 waitKeys()
